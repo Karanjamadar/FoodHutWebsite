affb577758d80144188702f35cef7e64a77bea15
Sakshi Ingale <sakshiingale2101@gmail.com>
2023-02-18 21:40:20 +0530

    Sakshi is very hungry want to eat something that doesn't available on this website

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -18,16 +18,13 @@ const FoodDetails = () => {
   return (
     <>
       <div className=" d-flex justify-content-center p-4 row mt-5 pt-5 header">
-        {/* {
+        {
           isloading ? <div className="spinner-border text-success mx-auto my-auto" style={{ height: "100px", width: "100px"}} role="status">
             <span className="sr-only">Loading...</span>
-          </div> : */}
+          </div> : 
             <div key={food?.id}>
                 <div className='gallary-item wow fadeIn border border-white rounded h-100 mb-1 mt-0 bg-body b bg-secondary' >
                   <img src={food?.image} alt="failed to load" className="gallary-img" style={{height:400, marginLeft:30}} />
-                  {/* <Link to={`/details/${item.id}`} className="gallary-overlay" >
-                    <h4 className="text-white">{item?.name}</h4>
-                  </Link> */}
                   <div style={{paddingLeft:100, paddingRight:100,marginLeft:100}}>
                     <h4>{food?.name}</h4>
                     <p className="text-white">ingredients : [ {
@@ -54,7 +51,7 @@ const FoodDetails = () => {
                 </div>
               </div>
 
-        {/* } */}
+        } 
       </div>
     </>
   )
